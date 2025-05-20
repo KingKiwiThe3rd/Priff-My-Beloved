19e8047cb3de592b61512d2d2d368fa9e1d38724
AhsRoz <dvirkallush07@gmail.com>
2025-05-20 13:57:20 +0300

    Fixed paralaxing

--- scenes/parallax_layer_2.gd
+++ scenes/parallax_layer_2.gd
@@ -1,19 +1,34 @@
 extends ParallaxLayer
+
 @onready var sprite = $Sprite2D
 
 func _ready():
 	var screen_size = get_viewport().size
-	var zoom_factor = 3.0 # Camera zoom from Inspector
+	var camera_zoom = 3.0 # Camera is zoomed 3x
+	var sprite_scale = 1.0 / camera_zoom # Scale sprite down to compensate
 	var texture_size = sprite.texture.get_size()
-	sprite.scale = Vector2(1.0 / zoom_factor, 1.0 / zoom_factor) # 0.333 for 1152x648
-	sprite.position = screen_size / (2 * zoom_factor) # (192, 108)
-	motion_scale = Vector2(0.5, 0) # Scroll slower than camera
-	motion_mirroring = Vector2(texture_size.x, 0) # Repeat at texture width
+	print(texture_size)
+	
+	# Scale sprite down by 1/3 to compensate for camera zoom
+	sprite.scale = Vector2(sprite_scale, sprite_scale)
+	
+	# Center vertically in the visible area
+	sprite.position = Vector2(screen_size.x/camera_zoom/2, screen_size.y /camera_zoom/2)
+	
+	# Scroll slower than camera
+	motion_scale = Vector2(0.5, 0)
+	
+	# Motion mirroring needs to use the effective width after scaling
+	# Since the sprite is scaled down, we need unscaled mirroring
+	motion_mirroring = Vector2(texture_size.x/3, 0)
+	
 	sprite.visible = true
 	sprite.modulate = Color(1, 1, 1, 1)
 	z_index = -1
+	
 	print("Viewport Size:", screen_size)
 	print("Texture Size:", texture_size)
+	print("Camera Zoom:", camera_zoom)
 	print("Sprite Scale:", sprite.scale)
 	print("Sprite Position:", sprite.position)
 	print("Motion Mirroring:", motion_mirroring)
