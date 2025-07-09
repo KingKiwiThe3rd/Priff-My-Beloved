3dc4d1952df0467d033ea3fbd7056697a37fa1a7
KingKiwiThe3rd <Akivaeagle12@gmail.com>
2025-07-10 01:56:11 +0300

    added dialog manager but didnt test it so hoplfly it works?

--- scripts/sign.gd
+++ scripts/sign.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var collision_shape_2d: CollisionShape2D = $InteractionArea/CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite2D
 
 const lines: Array[String] =[
 	"Kys"
@@ -11,8 +12,8 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
 
-#func _oninteract():
-	#DialogManager.start_dialog(global_position, lines, speech_sound)
-	#sprite.flip_h = true if interation_area.get_overlapping_boddies()[0].global_position.x < globalposition.x else false
-	#await DialogManager.dialog_finished
-	#
+func _on_interact():
+	DialogManager.start_dialog(global_position, lines)
+	sprite.flip_h = true if interaction_area.get_overlapping_boddies()[0].global_position.x < global_position.x else false
+	await DialogManager.dialog_finished
+	
